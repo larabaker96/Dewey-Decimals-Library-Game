3c17108f6e4c4054f1b642835fce5db1a800e0b4
Lara <18002529@vcconnect.co.za>
2020-11-02 20:53:27 +0200

    added tree node structure and research doc

---
+++ 18002529_PROG7312_POE/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18002529_PROG7312_Task2
+{
+    class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+    }
+}
---
+++ 18002529_PROG7312_POE/TreeNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18002529_PROG7312_Task2
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+
+        public TreeNode<T> Parent { get; set; }
+
+        public List<TreeNode<T>> Children { get; set; }
+    }
+}
